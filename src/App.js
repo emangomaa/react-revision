667fc13d6223688aa45c44acba790f8acf1a24bc
emangomaa <emangomaa149@gmail.com>
2022-10-15 20:40:17 +0200

    create parent and child component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 // import Great from './components/Great';
 // import Counter from './components/Counter';
-import Bindevents from './components/Bindevents';
+// import Bindevents from './components/Bindevents';
+import Parent from './components/methods as a props/Parent';
 function App() {
   return (
     <div className="App">
-      <Bindevents/>
+      <Parent/>
+      {/* <Bindevents/> */}
       {/* <Great/> */}
       {/* <Counter/> */}
     </div>
